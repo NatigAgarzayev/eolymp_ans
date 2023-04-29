d0aa782653801e7fde698c6fe7a68297b49712e0
natigGit <moothman@inbox.ru>
2023-04-29 19:31:09 +0400

    4281

---
+++ tasks.cpp
@@ -0,0 +1,16 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+using namespace std; 
+int main (){ 
+	string s; 
+	getline(cin, s); 
+	for(int i=0; i<s.length(); i++){ 
+		if(s[i] == tolower(s[i])){
+			s[i] = toupper(s[i]);
+		}
+		else{
+			s[i]=tolower(s[i]); 	
+		}
+		cout<<s[i]; 
+	} 
+}
